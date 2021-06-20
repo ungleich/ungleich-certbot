8dcf77335cecb629fc8d7e539138a76724b59811
Nico Schottelius <nico@nico-notebook.schottelius.org>
2021-06-20 14:36:38 +0200

    Correct permissions of generated certificates

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,3 +14,10 @@ fi
 certbot certonly --agree-tos --cert-name "${DOMAIN}" \
         --email "$EMAIL" --expand --non-interactive \
         --domain "$DOMAIN" --standalone $STAGING
+
+# Correct permissions for multi user container/pod deployments
+# if not indicated otherwise
+if [ -z "$LEAVE_PERMISSIONS_AS_IS" ]; then
+    find /etc/letsencrypt -type d -exec chmod 0755 {} \;
+    find /etc/letsencrypt -type f -exec chmod 0644 {} \;
+fi

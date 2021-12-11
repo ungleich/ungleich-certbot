74bbf62c21867a360bfb3a3920db98524be0aa6f
Nico Schottelius <nico@nico-notebook.schottelius.org>
2021-12-11 19:16:14 +0100

    push to local registry
    Signed-off-by: Nico Schottelius <nico@nico-notebook.schottelius.org>

--- build.sh
+++ build.sh
@@ -9,5 +9,7 @@ while [ $# -ge 1 ]; do
     git tag -a -m "Version $tag" $tag
     git push --tags
     docker tag ungleich-certbot:latest ungleich/ungleich-certbot:${tag}
+    docker tag ungleich-certbot:latest harbor.ungleich.svc.p10.k8s.ooo/ungleich-public/ungleich-certbot:${tag}
     docker push ungleich/ungleich-certbot:${tag}
+    docker push harbor.ungleich.svc.p10.k8s.ooo/ungleich-public/ungleich-certbot:${tag}
 done

39a119175f22382e8eed52a42863f844a916ae30
Nico Schottelius <nico@nico-notebook.schottelius.org>
2021-12-21 16:25:17 +0100

    update build.sh for pushing to harbor

--- build.sh
+++ build.sh
@@ -10,4 +10,9 @@ while [ $# -ge 1 ]; do
     git push --tags
     docker tag ungleich-certbot:latest ungleich/ungleich-certbot:${tag}
     docker push ungleich/ungleich-certbot:${tag}
+
+    # ungleich registry, slowly superseeding hub.docker.com
+    url=harbor.ungleich.svc.p10.k8s.ooo/ungleich-public/ungleich-certbot
+    docker tag ungleich-certbot:latest ${url}:${tag}
+    docker push ${url}:${tag}
 done
